b1a526c06854d280a802f1877347c70503f63cf6
Eric Soroos <eric-github@soroos.net>
2021-01-29 17:50:56 +0100

    ensure that we're returning all of the items

--- api/expander.py
+++ api/expander.py
@@ -4,15 +4,16 @@ def reshape(iterable, shapefunc):
     for elt in iterable:
         yield(shapefunc(elt))
 
-def expand(iterable, default=None):
+def expand(iterable, default=None, max_index=None):
     """
     Iterable should be a list of (index, value) tuples corresponding to
     a sparse vector.
-    
+
     will return an iterable of the dense matrix, with default values
-    for the missing. 
+    for the missing.
 
     e.g. iter= (1,3), (3,4) => None, 3, None 4
+    max_index will ensure that there are items up to that index.
     """
     last_index = -1
     for index, value in iterable:
@@ -20,6 +21,11 @@ def expand(iterable, default=None):
             yield default
         last_index = index
         yield value
+
+    # last index might not be the last of the vector.
+    if max_index:
+        for _ in range(max(0, max_index - last_index)):
+            yield default
 
 
 if __name__=='__main__':
